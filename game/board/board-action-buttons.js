334eecc9385228ca36d8682494e1476620f7af31
WojTechM <wojciech.m1997@gmail.com>
2019-04-18 12:06:05 +0200

    Resolve Media-Type discrepancies

--- game/board/board-action-buttons.js
+++ game/board/board-action-buttons.js
@@ -4,7 +4,7 @@ const c = require('../ship/ship-class');
 const mark = require('./board-marks');
 const buttons = require('../buttons-helper')
 const $ = require('jQuery');
-
+const id = Math.floor(Math.random() * 99999999); 
 let ship = c.Ship();
 
 document.getElementById('button_shoot').onclick = () => {
@@ -15,47 +15,12 @@ function fire() {
     mark.fire(field.chosenFieldToFire(), false);
     console.log(`FIRE :: ${field.chosenFieldToFire()} PACH PACH`);
     //buttons.disable('board_action_buttons', true);
-    
-    let x = parseInt(1);
+
+    let request = {"fieldNumber": field.chosenFieldToFire(), "playerId": id};
     // //test
-    $.post('http://localhost:8080/shot', "x", () => {
-        console.log(`dupa`)
+    $.post('http://localhost:8080/shot', request, () => {
         console.log(res)
     });
-
-
-
-    // request.post('http://localhost:8080/shot',1 , (error, res, body) => {
-    //     if (error) {
-    //       console.error(error)
-    //       return
-    //     }
-    //     console.log(`statusCode: ${res.statusCode}`)
-    //     console.log(body)
-    //   })
-
-
-    // const request = require('request')
-
-    // request.post('http://localhost:8080/shot', 1,
-    //     (error, res, body) => {
-    //         if (error) {
-    //             console.error(error)
-    //             return
-    //         }
-    //         console.log(`statusCode: ${res.statusCode}`)
-    //         console.log(res)
-    //     })
-
-
-    // $.ajax({
-    //     url         : "http://localhost:8080/shot", //wymagane, gdzie się łączymy
-    //     method      : "post", //typ połączenia, domyślnie get
-    //     contentType : 'application/json', //gdy wysyłamy dane czasami chcemy ustawić ich typ
-    //     dataType    : 'json', //typ danych jakich oczekujemy w odpowiedzi
-    //     data        : 1
-    //     });
-    
 }
 
 
