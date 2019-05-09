ccbe0ba5b5cf41afbe58c186a96ea8084143e00b
Kamil Rojek <kamroj@gmail.com>
2019-05-09 17:24:03 +0200

    Add changing theme //todo making it better

---
+++ game/board/board-theme.js
@@ -0,0 +1,38 @@
+//imports
+const $ = require('jQuery');
+
+let isDarkTheme = true;
+
+document.getElementById(`button_change_theme`).onclick = () => {
+    changeTheme();
+}
+
+function changeTheme() {
+    isDarkTheme ? loadLightThemeSettings() : loadDarkThemeSettings();
+}
+
+function loadLightThemeSettings() {
+    console.log("Load light theme!");
+    $('body').css({
+        'background': '#eeeded'
+    })
+
+    $('button').css({
+        'color': 'black',
+        'border': '1px solid black'
+    })
+    isDarkTheme = false;
+}
+
+function loadDarkThemeSettings() {
+    console.log("Load dark theme!");
+    $('body').css({
+        'background': '#151519'
+    })
+
+    $('button').css({
+        'color': '#ccc',
+        'border': '1px solid #045bbe'
+    })
+    isDarkTheme = true;
+}
