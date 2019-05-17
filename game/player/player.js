33447373ffcad8c0f7c4de430e5cf93b677fa091
WojTechM <wojciech.m1997@gmail.com>
2019-05-17 09:03:02 +0200

    Player id is now saved in localStorage

--- game/player/player.js
+++ game/player/player.js
@@ -1,37 +1,24 @@
 const communication = require('../communication-server-client');
 const buttons = require('../buttons-helper');
-const mark = require('../board/board-marks');
 
 /**
- * Generate randomly player id
+ * Load id from local storage (browser memory), and if it doesn't exist, generate it randomly
  */
-const id = Math.floor(Math.random() * 100000000);
-
-//do wywalenia
-let ships;
-
-// register yourself as a player on the server
-// communication.post("register", id, value => {
-//     ships = value;
-// });
+var id = localStorage.getItem("id");
+if (id == null) {
+    id = Math.floor(Math.random() * 100000000);
+    localStorage.setItem("id", id)
+}
 
 /**
  * Ask server if it is my turn
  */
 function isMyTurn() {
     communication.get(`turn`, id, result => {
-        console.log(`Is my turn: ${result}`);
         buttons.disable('board_action_buttons', !result);
-
-        // if (result) {
-        //     communication.get(`summary`, id, opponentShots => {
-        //         opponentShots.getShotResults().forEach(shot => {
-        //             mark.opponentShots(shot.getField());
-        //         })
-        //     })
-        // }
     })
 }
+
 setInterval(isMyTurn, 3000);
 
 module.exports = {
@@ -40,8 +27,5 @@ module.exports = {
     },
     refresh : () => {
         isMyTurn();
-    },
-    getShips : () => {
-        return ships;
     }
 }
