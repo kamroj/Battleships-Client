8f2615870714ec4666f88e3b45428d25f6a2b82d
atrzewik <42387362+atrzewik@users.noreply.github.com>
2019-05-23 16:49:24 +0200

    Correct roomList creation

--- game/room-list/room-list.js
+++ game/room-list/room-list.js
@@ -22,28 +22,30 @@ function getRoomsList() {
 }
 
 function createRoomList() {
-    roomsList.forEach(roomId => {
-        let button = document.createElement("button");
-        let buttonDiv = document.getElementById("sub_div_buttons_column");
-        let roomNameDiv = document.createElement("div");
-        let roomNumberDiv = document.createElement("div");
+    if( roomsList != "undefined"){
+        roomsList.forEach(roomId => {
+            let button = document.createElement("button");
+            let buttonDiv = document.getElementById("sub_div_buttons_column");
+            let roomNameDiv = document.createElement("div");
+            let roomNumberDiv = document.createElement("div");
 
-        button.className = 'ROOMS_BUTTON'
+            button.className = 'ROOMS_BUTTON'
 
-        roomNameDiv.className = "ROOMS";
-        roomNameDiv.innerHTML = `Room: `
+            roomNameDiv.className = "ROOMS";
+            roomNameDiv.innerHTML = `Room: `
 
-        roomNumberDiv.className = 'ROOMS_NUMBERS';
-        roomNumberDiv.innerHTML = roomId;
+            roomNumberDiv.className = 'ROOMS_NUMBERS';
+            roomNumberDiv.innerHTML = roomId;
 
-        button.appendChild(roomNameDiv);
-        button.appendChild(roomNumberDiv);
+            button.appendChild(roomNameDiv);
+            button.appendChild(roomNumberDiv);
 
-        button.id = roomId;
-        button.type = `list_enabled`;
+            button.id = roomId;
+            button.type = `list_enabled`;
 
-        buttonDiv.appendChild(button);
-    })
+            buttonDiv.appendChild(button);
+        })
+    }
 };
 
 function loadRoomListeners() {
