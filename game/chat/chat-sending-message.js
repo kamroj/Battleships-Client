58c44c7f461b3e862fe980b1480e6ed78d610b84
atrzewik <42387362+atrzewik@users.noreply.github.com>
2019-05-24 09:17:46 +0200

    Add sending message by pressing enter

--- game/chat/chat-sending-message.js
+++ game/chat/chat-sending-message.js
@@ -1,9 +1,24 @@
+const $ = require('jQuery');
 const player = require('../player/player');
 const communication = require('../communication-server-client');
 
 let textbox;
 
 document.getElementById('SEND_MESSAGE').onclick = () => {
+    sendText();
+}
+
+$(document).ready(function () {
+
+    $('#textbox').keypress(function (e) {
+        if (e.which == 13) {
+            sendText();
+        }
+    });
+    
+});
+
+function sendText() {
     textbox = document.getElementById('textbox');
     getText();
 }
